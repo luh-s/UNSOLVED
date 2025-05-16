34afa66671e54d9643b9192d62701af7d1b775c3
Luan <luan.azul120107@gmail.com>
2025-05-16 14:37:56 -0300

    Add files via upload

--- UNSOLVED.c
+++ UNSOLVED.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <conio.h>
+#include <time.h>
 
 void title()
 {
@@ -17,24 +19,91 @@ void pause()
     printf("\n\t\t\t\t\t[APERTE QUALQUER TECLA PARA CONTINUAR]\n");
     system("pause > NUL");
 }
+void map(int v1, int v2)
+{
+    int i,i2,x=v1/2,y=v2/2;
+    char mapa[v1][v2],movimento;
+    for (i = 0; i < v1; i++)
+    {
+        for ( i2 = 0; i2 < v2; i2++)
+        {
+            if (i==0 || i==v1-1 || i2==0 || i2==v2-1)
+            {
+                mapa[i][i2]='#';
+            }
+            else
+            {
+                mapa[i][i2]=' ';
+            }
+        }
+        
+    }
+    mapa[1][2]='@';
+    mapa[x][y]='&';
+    while (i!=-1)
+    {
+        system("cls");
+        for (i = 0; i < v1; i++)
+        {
+            printf("\n\t\t\t\t");
+            for (i2 = 0; i2 < v2; i2++)
+            {
+                printf("%c",mapa[i][i2]);   
+            }
+            
+        }
+        movimento=getch();
+        mapa[x][y]=' ';
+        if (movimento=='w')
+        {
+            if (mapa[x-1][y]!='#')
+            {
+                x--;
+            }   
+        }
+        else if (movimento=='a')
+        {
+            if (mapa[x][y-1]!='#')
+            {
+                y--;
+            }   
+        }
+        else if (movimento=='s')
+        {
+            if (mapa[x+1][y]!='#')
+            {
+                x++;
+            }   
+        }
+        else if (movimento=='d')
+        {
+            if (mapa[x][y+1]!='#')
+            {
+                y++;
+            }   
+        }
+        else if (movimento=='i')
+        {
+            if (mapa[x-1][y]=='@')
+            {
+                printf("interagiu");
+                pause();
+            }
+            
+        }
+        
+        mapa[x][y]='&';
+    }
+}
 
 int main()
 {
     char nome[20];
-    int escolha=0,mapa[20][20];
+    int escolha=0;
 
     UINT portugues = 65001;
     SetConsoleOutputCP(portugues);
 
-    int i,i2;
-    for ( i = 0; i < 20; i++)
-    {
-        for ( i2 = 0; i2 < 20; i2++)
-        {
-            mapa[i][i2]=0;
-        }
-        
-    }
     title();
     pause();
 
@@ -43,10 +112,10 @@ int main()
         system("cls");
         title();
         printf("\t\t\t\t\t\t[ESCOLHA UMA DAS OPÇÕES]\n\n\t\t\t\t\t\t[1] - JORNADA\n\t\t\t\t\t\t[2] - CRÉDITOS\n\t\t\t\t\t\t[3] - SAIR DE UNSOLVED\n\t\t\t\t\t\t ");
-        scanf("%i",&escolha);
+        escolha=getch();
 
         
-        if (escolha==1)
+        if (escolha=='1')
         {
             system("cls");
             printf("\t\t\t\tApós anos de investigação, o departamento da verdade encerrou\n\t\t\t\t o caso o qual você tentou encontrar as respostas.\n\n\t\t\t\tVocê acreditava que estava cada vez mais perto de uma solução,\n\t\t\t\t sendo assim, decidiu fazer isso por conta própria.\n\n");
@@ -55,7 +124,7 @@ int main()
             system("cls");
             escolha++;
         }
-        else if (escolha==2)
+        else if (escolha=='2')
         {
             system("cls");
             printf("\t\t\t\t\t\t\tCRÉDITOS:\n\n\t\t\t\t\t\tLUAN PIEDADE DE OLIVEIRA\n");
@@ -63,7 +132,7 @@ int main()
             escolha=0;
 
         }
-        else if (escolha==3)
+        else if (escolha=='3')
         {
             printf("\n\n\t\t\t\t\t\tOBRIGADO POR JOGAR!\n\n\n\n\n\n\n\n");
             goto fim;
@@ -127,27 +196,8 @@ int main()
     pause();
     system("cls");
 
-    for ( i = 0; i < 20; i++)
-    {
-        printf("\n\t\t\t\t");
-
-        for ( i2 = 0; i2 < 20; i2++)
-        {
-            if (mapa[i][i2]==0)
-        {
-            printf("-");
-        }
-        else
-        {
-            printf("*");
-        }
-
-        }
-        
-    }
+    map(10,20);
     
-
-
     fim:
     return 0;
 }
